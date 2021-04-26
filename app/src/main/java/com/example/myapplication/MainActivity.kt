d185df9270fe92d599495149ad2ead6e7c2f7bf1
Jociel Anne A. Lucido <lucidojocielanne@gmail.com>
2021-04-26 12:47:47 +0900

    dateandtime

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+
+import  java.time.LocalDate
+import java.time.LocalTime
+
+fun main(args: Array<String>) {
+   val current = LocalDate.now()
+   val time = LocalTime.now()
+
+    println(current)
+    println(time)
+
+}
+
